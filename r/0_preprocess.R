5a934aaf6f4479f793cef5ad27c2d7c2c28b4f84
Ty Stanford <tystan@gmail.com>
2023-06-15 22:06:08 +0930

    Analysis data preparation

---
+++ r/0_preprocess.R
@@ -0,0 +1,303 @@
+
+# ---- libs ----
+
+
+suppressPackageStartupMessages({
+  library("readr")
+  library("dplyr")
+  library("tidyr")
+  library("lubridate") # way to handle dates better than default R way
+  library("ggplot2") 
+  library("ggrepel") 
+  library("knitr")
+  library("foreach")
+  library("arrow") # read/write parquet files
+})
+
+
+
+# here are the functions written for this project
+source("../r/_funcs.R")
+
+
+# ---- consts ----
+
+# arbitrarily, let's go with minimum cell count of 1 
+arbitrary_cell_min <- 1
+
+# these are the thresholds for pain_topic to be pain == TRUE
+# thresholds <- c(0.010, 0.025, 0.05, 0.075, 0.100) 
+thresholds <- seq(0.010, 0.100, by = 0.005) #, 0.150) 
+
+
+col_pal <- c("cyan4", "darkorange", "purple", "dodgerblue")
+
+
+target_lst <-
+  list(
+    "pelvic_mesh",
+    "pelvic_mesh",
+    "pelvic_mesh",
+    "hernia_mesh",
+    c("hernia_mesh", "other_mesh")
+  )
+
+compar_lst <-
+  list(
+    "hernia_mesh",
+    c("hernia_mesh", "other_mesh"),
+    c("hernia_mesh", "other_mesh", "other_device"),
+    "other_mesh",
+    "other_device"
+  )
+
+
+
+
+# ---- load_dat ----
+
+
+clean_data_cols <-
+  cols(
+    Report_ID = col_double(),
+    Date = col_date(format = ""),
+    pain_word = col_logical(),
+    pain_topic = col_double(),
+    type = col_character()
+  )
+
+clean_data <- read_csv("../dat/clean_data.csv", col_types = clean_data_cols)
+
+
+# ---- check_dups ----
+
+
+### all look like duplicates
+inner_join(
+  clean_data,
+  clean_data %>% 
+    group_by(Report_ID) %>% 
+    summarise(n = n(), .groups = "drop") %>% 
+    dplyr::filter(n > 1),
+  "Report_ID"
+) %>%
+  arrange(Report_ID) %>%
+  print(., n = nrow(.))
+
+
+# make dup free
+clean_data <-
+  clean_data %>%  
+  arrange(Report_ID, Date, desc(pain_word)) %>% # pain first in dups
+  group_by(Report_ID) %>% 
+  dplyr::filter(row_number() == 1) %>%
+  ungroup(.) %>%  
+  arrange(Date, Report_ID, desc(pain_word), desc(pain_topic))
+
+
+clean_data %>%
+  dplyr::filter(type == "other_mesh") %>%
+  # select(Report_ID) %>%
+  write_csv("../out/other_mesh_ids.csv")
+
+
+
+# ---- inspect ----
+
+
+cat("First 10 rows of raw data\n")
+clean_data %>%
+  arrange(Date) %>%
+  dplyr::filter(row_number() < 11) %>%
+  kable(.)
+
+# clean_data <-
+#   clean_data %>%
+#   dplyr::filter(
+#     type %in% c("pelvic_mesh", "hernia_mesh")
+#   )
+
+
+clean_data %>%
+  with(., table(type, pain_word)) %>%
+  knitr::kable(.)
+
+clean_data %>%
+  with(., table(type, pain_topic >= 0.05)) %>%
+  knitr::kable(.)
+
+
+
+# These are the device groups and subgroups.
+clean_data %>% 
+  group_by(type) %>% 
+  summarise(count = n()) %>%
+  kable(.)
+
+cat("\n\n## Histogram of `pain_word` (boolean) v `pain_topic` (score)")
+
+clean_data %>%
+  ggplot(., aes(pain_topic, fill = pain_word)) +
+  geom_histogram(bins = 30) +
+  scale_fill_manual(values = col_pal[1:2]) +
+  theme_bw()
+
+clean_data %>% 
+  dplyr::filter(
+    type %in% c("pelvic_mesh", "hernia_mesh", "other_mesh", "other_device")
+  ) %>%
+  ggplot(., aes(pain_topic, fill = pain_word)) +
+  geom_histogram(bins = 30) +
+  scale_fill_manual(values = col_pal[1:2]) +
+  facet_wrap(~ type, scales = "free_y") +
+  theme_bw()
+
+
+# ---- create_analysis_data ----
+
+
+### testing: example 1
+# Use pelvic mesh as group 1 and hernia_mesh mesh devices as group 2. 
+# The value of interest is the pain topic, being above the threshold of 0.05. 
+# (i.e. 5% of the document contains words from the pain topic)
+# You can adjust the topic threshold if you want to balance the groups more. 
+# A higher topic_threshold will look for documents that discuss "pain" more, and 
+# hence find less pain documents.
+
+# get_signal_dat(
+#   g1 = "pelvic_mesh",
+#   g2 = "hernia_mesh",
+#   pain_type = "pain_topic", 
+#   thresh = 0.05,
+#   cell_min = 1,
+#   cumul = TRUE,
+#   verbose = FALSE
+# ) %>%
+#   bind_cols(., thresh = 0.05)
+
+
+
+# takes ~ 20 sec
+cumul_dat <-
+  foreach(i = 1:length(target_lst), .combine = bind_rows, .packages = "dplyr") %do% {
+    foreach(th_j = thresholds, .combine = bind_rows, .packages = "dplyr") %do% {
+      
+      get_signal_dat(
+        g1 = target_lst[[i]],
+        g2 = compar_lst[[i]],
+        pain_type = "pain_topic", 
+        thresh = th_j,
+        cell_min = 1,
+        cumul = TRUE,
+        verbose = FALSE
+      ) %>%
+        mutate(
+          grps = 
+            paste0(
+              paste(target_lst[[i]], collapse = "/"), 
+              " v ",
+              paste(compar_lst[[i]], collapse = "/")
+            ),
+          dat_type = "cumulative", 
+          thresh = th_j
+        ) %>%
+          select(grps, dat_type, thresh, everything())
+      
+    }
+  }
+cumul_dat
+
+# takes ~ 20 sec
+snpsh_dat <-
+  foreach(i = 1:length(target_lst), .combine = bind_rows, .packages = "dplyr") %do% {
+    foreach(th_j = thresholds, .combine = bind_rows, .packages = "dplyr") %do% {
+      
+      get_signal_dat(
+        g1 = target_lst[[i]],
+        g2 = compar_lst[[i]],
+        pain_type = "pain_topic", 
+        thresh = th_j,
+        cell_min = 1,
+        cumul = FALSE,
+        verbose = FALSE
+      ) %>%
+        mutate(
+          grps = 
+            paste0(
+              paste(target_lst[[i]], collapse = "/"), 
+              " v ",
+              paste(compar_lst[[i]], collapse = "/")
+            ),
+          dat_type = "snapshot", 
+          thresh = th_j
+        ) %>%
+        select(grps, dat_type, thresh, everything())
+      
+    }
+  }
+snpsh_dat
+
+
+
+# ---- check_analysis_data ----
+
+nrow(cumul_dat)
+if (nrow(cumul_dat) != nrow(snpsh_dat)) {
+  stop("logic of creating analysis data producing different # rows in data")
+}
+
+
+chk_start_vals <- 
+  inner_join(
+    cumul_dat %>% 
+      group_by(grps, dat_type, thresh) %>% 
+      dplyr::filter(row_number() == 1) %>%
+      ungroup(.),
+    snpsh_dat %>% 
+      group_by(grps, dat_type, thresh) %>% 
+      dplyr::filter(row_number() == 1) %>%
+      ungroup(.),
+    c("grps", "thresh")
+  ) %>%
+    mutate(
+      mnth_same = (mnth.x == mnth.y),
+      counts_same = (nA.x = nA.y) & (nB.x = nB.y) & (nC.x = nC.y) & (nD.x = nD.y) 
+    )
+
+chk_start_vals %>%
+  select(grps, thresh, dat_type.x, dat_type.y, mnth_same, counts_same)
+
+with(chk_start_vals, table(mnth_same, counts_same, useNA = "ifany"))
+
+
+# check the first + second row in snapshot == second row in cumulative data 
+inner_join(
+  cumul_dat %>% 
+    group_by(grps, thresh) %>% 
+    dplyr::filter(row_number() %in% 1:2) %>%
+    ungroup(.),
+  snpsh_dat %>% 
+    group_by(grps, thresh) %>% 
+    dplyr::filter(row_number() %in% 1:2) %>%
+    ungroup(.),
+  c("grps", "thresh", "mnth")
+) 
+
+
+
+
+# ---- export ----
+
+
+# all spontaneous report analysis data
+sra_dat <- 
+  bind_rows(
+    cumul_dat,
+    snpsh_dat
+  )
+
+sra_dat %>%
+  write_parquet(., sink = "../dat/sra_dat.parquet")
+
+
+
---
+++ r/_funcs.R
@@ -0,0 +1,205 @@
+
+# ---- funcs ----
+
+# The below function is thanks to Curtis:
+# https://github.com/curtis-murray/MedicalDevicesNLP
+
+#' Function to get data for disproportionality analysis
+#' 
+#' @param group_1 any vector combination of "pelvic_mesh", "hernia_mesh",
+#' "other_mesh", "other_device".
+#' @param group_2 any vector combination of "pelvic_mesh", "hernia_mesh",
+#' "other_mesh", "other_device".
+#' @param pain_type either "pain_word" which detects if the word pain 
+#' appears in a doucment, or "pain_topic", which will detect the pain 
+#' topic in a document.
+#' @param topic_threshold if pain_type == "pain_topic" then this is the 
+#' value that the topic density must exceed for the topic to be registered.
+#'  The default value of 0.05 will count documents with at least 5% of words 
+#'  being from the pain topic.
+#' @return Tibble of counts nA nB nC nD
+#' @examples
+#' get_data(
+#'   group_1 = c("pelvic_mesh"), group_2 = c("hernia_mesh", "other_mesh"), 
+#'   pain_type = "pain_topic", topic_threshold = 0.05
+#' )
+#' get_data(
+#'   group_1 = c("pelvic_mesh"), group_2 = c("hernia_mesh"), 
+#'   pain_type = "pain_word", topic_threshold = 0.05
+#' )
+
+get_data2 <- function(group_1, group_2, pain_type, topic_threshold = 0.05){
+  
+  intersection = intersect(group_1, group_2)
+  if(length(intersection) > 0){
+    warning(paste(intersection, " contained in both groups", sep = ""))
+  }
+  dat_out <-
+    clean_data %>% 
+    filter(type %in% c(group_1,group_2)) %>% 
+    mutate(group = ifelse(type %in% group_1, "group_1", "group_2")) %>% 
+    mutate(pain_topic = pain_topic >= topic_threshold) 
+  
+  dat_out <- dat_out[, c("Report_ID", "Date", "group", pain_type)]
+  dat_out[["pain"]] <- dat_out[[pain_type]]
+  dat_out[[pain_type]] <- NULL # rename and remocve
+  
+  # select(Report_ID, Date, group, pain = !!as.name(pain_type)) %>% 
+  
+  dat_out <- dat_out %>%
+    mutate(group_tf = (group == "group_1")) %>% 
+    mutate(nA = 1*(group_tf & pain),
+           nB = 1*(group_tf & !pain),
+           nC = 1*(!group_tf & pain),
+           nD = 1*(!group_tf & !pain)) %>% 
+    select(-group_tf)
+  
+}
+
+
+# Ty Stanford (2022-05-24)
+
+get_signal_dat <- 
+  function(
+    g1 = "pelvic_mesh", 
+    g2 = c("hernia_mesh", "other_mesh"),
+    pain_type = "pain_topic", 
+    thresh = 0.05,
+    cell_min = 1,
+    cumul = TRUE, # want counts to that point in time?
+    verbose = FALSE
+  ) {
+    
+    
+    d_g1_v_g2 <- 
+      get_data2(
+        group_1 = g1, 
+        group_2 = g2, 
+        pain_type = pain_type, 
+        topic_threshold = thresh
+      ) %>%
+      mutate(Report_ID = as.character(Report_ID)) # make sure ID is discrete
+    
+    # check report_ids unique
+    ids_df <- d_g1_v_g2 %>% select(Report_ID)
+    ids <- pull(ids_df, Report_ID)
+    if (length(ids) != length(unique(ids))) {
+      message("The following IDs are repeated")
+      ids_df %>% 
+        group_by(Report_ID) %>% 
+        summarise(n = n(), .groups = "drop") %>% 
+        dplyr::filter(n > 1) %>%
+        print(.)
+      stop("duplicate IDs found")
+    }
+    
+    # sort by date, create "YYYY-MM" variable for grouping
+    d_g1_v_g2 <-
+      d_g1_v_g2 %>%
+      arrange(Date) %>%
+      mutate(mnth = paste0(year(Date), "-", sprintf("%02.0f", month(Date))))
+    
+    if (verbose) {
+      # peak
+      d_g1_v_g2 %>% kable(.)%>% print(.)
+    }
+    
+    # make each entry a monthly count
+    d_g1_v_g2 <-
+      d_g1_v_g2 %>%
+      group_by(mnth) %>%
+      summarise(across(matches("^n[A-D]"), sum)) %>%
+      ungroup()
+    
+    # d_g1_v_g2 %>% print(. , n = nrow(.))
+    
+    # now make all counts cumulative to that point in time
+    d_g1_v_g2_cumul <-
+      d_g1_v_g2 %>%
+      mutate(across(matches("^n[A-D]"), cumsum))
+    
+    if (verbose) {
+      # go on, have a peak at what we're lookin at
+      d_g1_v_g2 %>% kable(.) %>% print(.)
+      # stop()
+    }
+    
+    # now let's find a date with sufficient contingency counts as starting point
+    date_min_dat <-
+      d_g1_v_g2_cumul %>%
+      pivot_longer(cols = matches("^n[A-D]")) # long format for life
+    
+    
+    if (verbose) {
+      # go on, have a peak at what we're lookin at
+      date_min_dat %>% kable(.) %>% print(.)
+    }
+    
+    date_min_dat <-
+      date_min_dat %>%
+      dplyr::filter(value >= cell_min) %>%
+      # now need to find date when all nA to nD reach `arbitrary_cell_min`
+      # so get max of mins
+      group_by(name) %>%
+      summarise(mnth = min(mnth)) %>%
+      ungroup() 
+    
+    if (verbose) {
+      # go on, have a peak at what we're lookin at
+      date_min_dat %>% kable(.) %>% print(.)
+    }
+    
+    date_min <-
+      date_min_dat %>%
+      pull(mnth) %>%
+      max(.)
+    
+    if (verbose) {
+      print(date_min)
+      print(class(date_min))
+    }
+    
+    if (cumul) { # want cumulative counts
+      
+      d_g1_v_g2 <- 
+        d_g1_v_g2_cumul %>% 
+        dplyr::filter(mnth >= date_min) # update data to date range wanted
+      
+    } else {  # want snapshot counts (but need cumulative until min count met)
+      
+      d_g1_v_g2 <-
+        bind_rows(
+          d_g1_v_g2_cumul %>% dplyr::filter(mnth <= date_min) %>%
+            summarise(
+              mnth = max(mnth),
+              across(matches("^n[A-D]"), max)
+            ),
+          d_g1_v_g2 %>% dplyr::filter(mnth > date_min)
+        )
+      
+    }
+    
+    # d_g1_v_g2 <-
+    #   d_g1_v_g2 %>%
+    #   arrange(mnth)
+    
+    # return
+    return(d_g1_v_g2)
+    
+  }
+
+
+# Ty Stanford (2022-05-24)
+
+pretty_da <- function(mnth, thresh, nA, nB, nC, nD, alpha = 0.05, n_mcmc = 1e+05) {
+  
+  out_cols_of_interest <- c("est_name", "est_scale", "est", "ci_lo", "ci_hi")
+  da <- pharmsignal::bcpnn_mcmc_signal(nA, nB, nC, nD, alpha = alpha, n_mcmc = n_mcmc)
+  da <- da[, out_cols_of_interest]
+  da <- bind_cols(tibble(mnth = mnth), tibble(thresh = thresh), da)
+  return(da)
+  
+}
+
+
+
